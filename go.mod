b43eae2c86d3d6b9f79ca4b2593a3e5c4ab649d5
purushotham <purushotham.prathapa@gmail.com>
2021-07-04 23:15:26 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module testCICD
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("addition ", add(1, 2))
+}
+
+func add(i, j int32) int32 {
+
+	return i + j
+
+}
